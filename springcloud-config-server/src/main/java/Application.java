1d2749a1748d809a8536da649611b59ce9abf107
wuhaijun <wuhaijun@chtwm.com>
2018-01-04 13:58:48 +0800

    spring cloud 学习

---
+++ springcloud-config-server/src/main/java/Application.java
@@ -0,0 +1,15 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by wuhaijun on 2018/1/4.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class Application {
+    public static void main(String[] args){
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+}
